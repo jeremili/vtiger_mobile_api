a765c50d4adfae2fd82ad26f1c0e2498eeca29cd
jeremili <prevostjeremy@gmail.com>
2017-04-18 17:15:25 +0200

    add login() function

--- Vtiger_Mobile_Curl_Class.php
+++ Vtiger_Mobile_Curl_Class.php
@@ -7,14 +7,13 @@
 class Vtiger_Mobile_Curl_Class {
 	var $endpoint_url;
 	var $user_id;
-	var $user_name;
-	var $user_key;
+	var $user_name; // login
+	var $user_key; // password
 	var $token;
 	var $curl_handler;
 	
 	var $defaults = array(
 			CURLOPT_HEADER => 0,
-			// CURLOPT_FRESH_CONNECT => 1,
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_TIMEOUT => 10,
 			CURLOPT_SSL_VERIFYPEER => false,	// ssl fix
@@ -28,6 +27,29 @@ class Vtiger_Mobile_Curl_Class {
 		$this->user_name = $name;
 		$this->user_key = $key;
 		$this->token = 0;
+	}
+
+	function login() {
+		$curl_handler = curl_init();
+		
+		$params = array("_operation" => "login", "username" => $this->user_name, "password" => $this->user_key);
+		$options = array(CURLOPT_URL => $this->endpointUrl, CURLOPT_POST => 1, CURLOPT_POSTFIELDS => http_build_query($params));
+		curl_setopt_array($curl_handler, ($this->defaults + $options));
+		$result = curl_exec($curl_handler);
+		if (!$result) {
+			die(curl_error($curl_handler));
+		}
+		$jsonResponse = json_decode($result, true);
+
+		if ($jsonResponse["success"] == false) {
+			die("Login failed: ".$jsonResponse["error"]["message"]);
+		}
+		
+		$this->user_id = $jsonResponse["result"]["login"]["userid"];
+		$sessionId = $jsonResponse["result"]["login"]["session"];
+		//save session id
+		$this->token = $sessionId;
+		return true;
 	}
 }
 ?>
